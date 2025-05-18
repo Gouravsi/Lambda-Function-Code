0b76da5c9f5e2d029813673262bc394d1919e936
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 14:54:58 +0530

    Change the Api adress

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name        = "MyLambdaAPI"
+  name        = "UpdatedAPI"
   description = "API for Lambda functions"
 }
 
@@ -7,6 +7,22 @@ resource "aws_api_gateway_resource" "secret_lambda_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "secret_lambda"
+}
+
+resource "aws_api_gateway_method" "secret_lambda_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.secret_lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "secret_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.secret_lambda_resource.id
+  http_method             = aws_api_gateway_method.secret_lambda_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.secret_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_resource" "email_template_resource" {
@@ -22,20 +38,6 @@ resource "aws_api_gateway_method" "email_template_post" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "email_template_get" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.email_template_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "secret_lambda_get" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.secret_lambda_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_integration" "email_template_post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.email_template_resource.id
@@ -45,6 +47,13 @@ resource "aws_api_gateway_integration" "email_template_post_integration" {
   uri                     = aws_lambda_function.email_template.invoke_arn
 }
 
+resource "aws_api_gateway_method" "email_template_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.email_template_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "email_template_get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.email_template_resource.id
@@ -52,15 +61,6 @@ resource "aws_api_gateway_integration" "email_template_get_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.email_template.invoke_arn
-}
-
-resource "aws_api_gateway_integration" "secret_lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.secret_lambda_resource.id
-  http_method             = aws_api_gateway_method.secret_lambda_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.secret_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
