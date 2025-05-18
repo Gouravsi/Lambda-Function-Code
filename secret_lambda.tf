1d0125f19fc350fca4c13d4160797a95ec8d1f7c
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 13:47:13 +0530

    updated_funcation_name

--- secret_lambda.tf
+++ secret_lambda.tf
@@ -1,15 +1,11 @@
-resource "aws_iam_role" "secret_lambda_role" {
-  name = "secret_lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
-}
-
 resource "aws_lambda_function" "secret_lambda" {
   function_name = "secret_lambda"
   handler       = "main.lambda_handler"
   runtime       = "python3.9"
-  role          = aws_iam_role.secret_lambda_role.arn
+  role          = aws_iam_role.lambda1_role.arn
   filename      = "${path.module}/../builds/secret_lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/../builds/secret_lambda.zip")
+
   environment {
     variables = {
       SECRET_NAME = "your-secret-id"
