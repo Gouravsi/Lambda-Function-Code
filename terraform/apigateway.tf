69900c1bd17735af5ef132d803c0aa265670f248
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 17:44:48 +0530

    Changed updated in apigateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -3,9 +3,17 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "API for Lambda functions"
 }
 
-resource "aws_api_gateway_resource" "email_template_resource" {
+# Root /api resource
+resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "api"
+}
+
+# /api/email_template resource
+resource "aws_api_gateway_resource" "email_template_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.api_resource.id
   path_part   = "email_template"
 }
 
@@ -20,7 +28,7 @@ resource "aws_api_gateway_integration" "email_template_get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.email_template_resource.id
   http_method             = aws_api_gateway_method.email_template_get.http_method
-  integration_http_method = "POST" # Required for AWS Lambda proxy integration
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.email_template.invoke_arn
 }
@@ -28,7 +36,6 @@ resource "aws_api_gateway_integration" "email_template_get_integration" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.email_template_get_integration
-    # Add other integrations here if needed
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api.id
