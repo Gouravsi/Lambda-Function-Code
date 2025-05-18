d70a2f9cc98829fb00d8d0ac510e79eda17a7989
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 18:45:49 +0530

    Test

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,19 +29,3 @@ resource "aws_iam_role" "lambda2_role" {
     }]
   })
 }
-
-resource "aws_iam_role" "lambda3_role_alt" {
-  name = "lambda3-role-alt"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Effect = "Allow",
-      Sid       = ""
-    }]
-  })
-}
