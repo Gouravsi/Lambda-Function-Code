7df0a521694a9407926242ff65643109cf46a100
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 17:22:09 +0530

    Handling duplicate module name

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,35 @@
+resource "aws_iam_role" "lambda1_role" {
+  name = "lambda1-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid       = ""
+    }]
+  })
+}
+
+resource "aws_iam_role" "lambda2_role" {
+  name = "lambda2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid       = ""
+    }]
+  })
+}
+
 resource "aws_iam_role" "lambda3_role_alt" {
   name = "lambda3-role-alt"
 
