79e727febf3ce575d521da2d35b3e7d1433aada2
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 19:31:06 +0530

    test

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# IAM role for Lambda execution
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
   assume_role_policy = jsonencode({
@@ -39,23 +40,27 @@ resource "aws_iam_role" "lambda_exec" {
   })
 
   lifecycle {
+    # This prevents errors if the role name changes outside Terraform
     ignore_changes = [
-      name, # ignore name changes so it won't error if the role exists with this name
+      name,
     ]
   }
 }
 
+# Attach basic Lambda execution role policy for CloudWatch logging
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Package your lambda.py into a zip
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "${path.module}/lambda/main.py"
   output_path = "${path.module}/lambda_function_payload.zip"
 }
 
+# The Lambda function resource
 resource "aws_lambda_function" "my_lambda" {
   function_name    = "my_lambda"
   filename         = data.archive_file.lambda_zip.output_path
@@ -68,11 +73,13 @@ resource "aws_lambda_function" "my_lambda" {
   depends_on = [aws_iam_role_policy_attachment.lambda_logs]
 }
 
+# API Gateway HTTP API
 resource "aws_apigatewayv2_api" "api" {
   name          = "lambda-api"
   protocol_type = "HTTP"
 }
 
+# API Gateway Integration with Lambda
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.api.id
   integration_type       = "AWS_PROXY"
@@ -81,18 +88,21 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   payload_format_version = "2.0"
 }
 
+# Route for GET /
 resource "aws_apigatewayv2_route" "lambda_route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
+# Default stage with auto deployment enabled
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
 }
 
+# Permission to allow API Gateway to invoke Lambda
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -101,6 +111,7 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
+# Output the API Gateway URL
 output "api_gateway_url" {
   description = "Public URL to access the Lambda function via API Gateway"
   value       = aws_apigatewayv2_api.api.api_endpoint
