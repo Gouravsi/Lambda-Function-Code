bd39e2a5d8d1967b0f6e7c90dcd5dc0d9d12d89a
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 14:47:58 +0530

    Added Error handling in email api

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -15,10 +15,10 @@ resource "aws_api_gateway_resource" "email_template_resource" {
   path_part   = "email_template"
 }
 
-resource "aws_api_gateway_method" "secret_lambda_get" {
+resource "aws_api_gateway_method" "email_template_post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.secret_lambda_resource.id
-  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.email_template_resource.id
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -27,6 +27,31 @@ resource "aws_api_gateway_method" "email_template_get" {
   resource_id   = aws_api_gateway_resource.email_template_resource.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "secret_lambda_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.secret_lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "email_template_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.email_template_resource.id
+  http_method             = aws_api_gateway_method.email_template_post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.email_template.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "email_template_get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.email_template_resource.id
+  http_method             = aws_api_gateway_method.email_template_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.email_template.invoke_arn
 }
 
 resource "aws_api_gateway_integration" "secret_lambda_integration" {
@@ -38,19 +63,11 @@ resource "aws_api_gateway_integration" "secret_lambda_integration" {
   uri                     = aws_lambda_function.secret_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "email_template_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.email_template_resource.id
-  http_method             = aws_api_gateway_method.email_template_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.email_template.invoke_arn
-}
-
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.secret_lambda_integration,
-    aws_api_gateway_integration.email_template_integration
+    aws_api_gateway_integration.email_template_post_integration,
+    aws_api_gateway_integration.email_template_get_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api.id
