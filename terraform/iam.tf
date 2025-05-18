d8fe0d69a5e0e458ced07900396030a68d7e1fec
Gourav Singh <gouravsingh750850@gmail.com>
2025-05-18 14:01:02 +0530

    added policy in iam file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,26 +1,59 @@
-data "aws_iam_policy_document" "lambda_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
+resource "aws_iam_role" "lambda1_role" {
+  name = "lambda1-role"
 
-resource "aws_iam_policy" "secret_lambda_policy" {
-  name   = "secret_lambda-policy"
-  policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect   = "Allow",
-      Action   = ["secretsmanager:GetSecretValue"],
-      Resource = "*"
+      Action    = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda1_attach" {
-  role       = aws_iam_role.secret_lambda.name
-  policy_arn = aws_iam_policy.secret_lambda_policy.arn
+resource "aws_iam_role" "lambda2_role" {
+  name = "lambda2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+# secret_lambda needs access to Secrets Manager
+resource "aws_iam_role_policy_attachment" "lambda1_secrets" {
+  role       = aws_iam_role.lambda1_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+# email_template needs access to SES GetTemplate
+resource "aws_iam_policy" "ses_get_template" {
+  name        = "LambdaEmailGetTemplate"
+  description = "Allows GetTemplate on SES"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ses:GetTemplate"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda2_ses_template" {
+  role       = aws_iam_role.lambda2_role.name
+  policy_arn = aws_iam_policy.ses_get_template.arn
 }
